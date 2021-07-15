025fe97da40ff0c720efb630fbf95aa48ad44889
Debarshi <debopiku1122@gmail.com>
2021-07-15 16:01:55 +0530

    initial

---
+++ orders.js
@@ -0,0 +1,8 @@
+module.exports.getOrders = async () => {
+  const response = {
+    statusCode: 200,
+    body: 'From Order Function'
+  };
+  return response;
+};
+
---
+++ products.js
@@ -0,0 +1,8 @@
+module.exports.getProducts = async () => {
+  const response = {
+    statusCode: 200,
+    body: 'From Products Function'
+  };
+  return response;
+};
+
